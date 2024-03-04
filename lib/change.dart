5e261f599802d8ae7c811cb90aad54d2e0417ecc
Deepak S <deepakthelakkad@gmail.com>
2024-03-04 15:35:29 +0530

    first commit

---
+++ lib/change.dart
@@ -0,0 +1,4 @@
+void main(){
+  print('Hello i am "johndoe"');
+  print("Hello i'm 'johndoe'");
+}
---
+++ lib/formula.dart
@@ -0,0 +1,12 @@
+double interest(int p, int t,int r)
+{
+double  formula =  p*t*r/100;
+   
+return formula;
+}
+
+void main()
+{
+    var output = interest(10, 20,30);
+    print(output);
+}
